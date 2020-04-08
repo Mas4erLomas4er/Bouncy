ac027f6ba5f17203286c6dee4a5a154c0ad6e052
mas4erlomas4er <i9997117@gmail.com>
2020-04-08 17:08:32 +0300

    fix URLs11

--- app/js/common.js
+++ app/js/common.js
@@ -16,7 +16,7 @@ $( document ).ready( function () {
 		let typeElem = new Typed( '.typed', {
 			strings : [ "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio... " ],
 			typeSpeed : 0,
-			showCursor : false
+			showCursor : true
 		} );
 	};
 
